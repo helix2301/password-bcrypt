234a3399b4e92ebda9fa0099dcea041766a928f0
Brandon Lipani (Lipani Security LLC) <brandon@lipanisecurity.com>
2024-04-18 11:07:40 -0400

    Update en.py

--- en.py
+++ en.py
@@ -7,5 +7,3 @@ for i in range(3):
     setpass = bytes(password, 'utf-8')
     hashed = bcrypt.hashpw(setpass, bcrypt.gensalt(10))
     print(hashed)
-
-#https://null-byte.wonderhowto.com/how-to/use-beginner-python-build-brute-force-tool-for-sha-1-hashes-0185455/
